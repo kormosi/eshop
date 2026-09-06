c1af935364115077b9c10aa692469500866e5df5
Patrik Kormosi <patres.kormi@gmail.com>
2026-09-06 21:41:32 +0200

    Inline OrderItems inside Order in admin panel

--- eshop/admin.py
+++ eshop/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from .models import Product, Order, OrderItem
 
 
-admin.site.register(Product)
-admin.site.register(Order)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "currency", "active")
+    list_filter = ("active",)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "status", "total", "created_at")
+    list_filter = ("status",)
+    inlines = [OrderItemInline]
+
+
 admin.site.register(OrderItem)
