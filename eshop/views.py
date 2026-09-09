c77bf28775f6c554e5b9bb40c460fefcf946e7a3
Patrik Kormosi <patres.kormi@gmail.com>
2026-09-09 21:37:30 +0200

    Some refactoring + work done on cart

--- eshop/views.py
+++ eshop/views.py
@@ -12,35 +12,9 @@ def home(request):
         {"product": product},
     )
 
-
-def checkout(request):
-    product = get_object_or_404(Product, active=True)
-    order = Order.objects.create(
-        email=request.POST["email"],
-        total=product.price,
-        currency=product.currency,
-    )
-    OrderItem.objects.create(
-        order=order,
-        product=product,
-        quantity=1,
-        unit_price=product.price,
-    )
-    session = create_checkout_session(order)
-    
-    order.stripe_checkout_session_id = session.id
-    order.save(update_fields=["stripe_checkout_session_id"])
-    return redirect(session.url)
-
-
-def checkout_success(request):
-    return render(request, "eshop/checkout_success.html")
-
-def checkout_cancel(request):
-    return render(request, "eshop/checkout_cancel.html")
-
 def cart(request):
     return render(request, "eshop/cart.html")
+
 
 def cart_data(request):
     ids = request.GET.get("ids", "")
@@ -65,3 +39,31 @@ def cart_data(request):
         }
         for product in products
     ], safe=False)
+
+def checkout(request):
+    product = get_object_or_404(Product, active=True)
+    order = Order.objects.create(
+        email=request.POST["email"],
+        total=product.price,
+        currency=product.currency,
+    )
+    OrderItem.objects.create(
+        order=order,
+        product=product,
+        quantity=1,
+        unit_price=product.price,
+    )
+    session = create_checkout_session(order)
+    
+    order.stripe_checkout_session_id = session.id
+    order.save(update_fields=["stripe_checkout_session_id"])
+    return redirect(session.url)
+
+
+def checkout_success(request):
+    return render(request, "eshop/checkout_success.html")
+
+
+def checkout_cancel(request):
+    return render(request, "eshop/checkout_cancel.html")
+
